968b27c62d8066cda22f5cd0c855ec7037672a8b
jesus-flores <chucho_quidproquo@hotmail.com>
2019-11-26 15:58:01 -0600

    profile preliminar faltan tags

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,8 +9,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 }, false);
-
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems, options);
-});
