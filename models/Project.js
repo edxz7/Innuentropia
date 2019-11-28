d667e44ffe00a3b9293362730f134a37c81b1d48
jesus-flores <chucho_quidproquo@hotmail.com>
2019-11-28 16:47:39 -0600

    Asuntes pequeños varios, faltan chips todavia

--- models/Project.js
+++ models/Project.js
@@ -1,22 +1,29 @@
-const { model, Schema } = require("mongoose");
-
-const projectSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["starting",],
-    }
+const {
+  model,
+  Schema
+} = require("mongoose");
+const projectSchema = new Schema({
+  title: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: true,
-    versionKey: false
+  description: {
+    type: String,
+    required: true,
+  },
+  status_project: {
+    type: String,
+    enum: ["Starting Adventure", "halfway through", "Advance stage", "Almost ready"],
+  },
+  req_skills: [String],
+  ending_date: Date,
+  external_url: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
   }
-);
+}, {
+  timestamps: true,
+  versionKey: false
+});
+module.exports = model("Project", projectSchema);
