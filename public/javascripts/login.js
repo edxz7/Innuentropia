800c7b309e9f5596cca922a9714871ebee24a141
jesus-flores <chucho_quidproquo@hotmail.com>
2019-11-28 02:45:07 -0600

    modales post con imagen, project, mejora en carusel

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, {
     duration: 200,
-    numVisible: 1
+    numVisible: 1,
+    fullWidth: true
   });
 });
